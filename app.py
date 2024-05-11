b7fe27494381cb442bbc3ed408689353b2986ce5
CharulRathore <rathore0ch@gmail.com>
2024-05-11 10:53:38 -0700

    minor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from flask_session import Session
+from urllib.parse import quote as url_quote
 import os
 import time
 
